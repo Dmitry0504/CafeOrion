52edb16bb3de567cd56cf0dc7edf783810aaac97
”Junior” <rip05-04-1990@yandex.ru>
2022-01-30 00:39:33 +0300

    adding handler for method argument not valid exception

--- src/main/java/com/orion/cafeorion/util/ExceptionHandlerAdvice.java
+++ src/main/java/com/orion/cafeorion/util/ExceptionHandlerAdvice.java
@@ -4,8 +4,14 @@ import com.orion.cafeorion.util.exсeption.NotFoundException;
 import com.orion.cafeorion.util.exсeption.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Dmitriy
@@ -15,7 +21,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Response> handleError(NotFoundException e) {
+    public ResponseEntity<Response> handleNotFoundException(NotFoundException e) {
         return new ResponseEntity<>(new Response(e.getMessage()), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException .class)
+    public ResponseEntity<Response> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrorList = e.getBindingResult().getFieldErrors();
+        Map<String, String> errorMessage = new HashMap<>();
+        for(FieldError f: fieldErrorList) {
+            errorMessage.put(f.getField(), f.getDefaultMessage());
+        }
+        return new ResponseEntity<>(new Response(errorMessage.toString()), HttpStatus.OK);
     }
 }
